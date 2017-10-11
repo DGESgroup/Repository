ad10bc34857148149f53666ff20e937103cdd8e7
ndurepo1 <26945714+ndurepo1@users.noreply.github.com>
2017-10-11 14:40:10 -0400

    Add files via upload

---
+++ Database Tables/DGES_Database.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Oct 11, 2017 at 08:34 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.9
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `DGES_Database`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Text_Files`
+--
+
+CREATE TABLE `Text_Files` (
+  `File_ID` int(200) NOT NULL,
+  `File_Name` varchar(500) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `File_Size` bigint(255) DEFAULT NULL,
+  `Content_Type` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `File_Extension` varchar(30) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `File_Content` varbinary(60000) DEFAULT NULL,
+  `Project_ID` int(200) NOT NULL,
+  `Document_Name` varchar(200) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Video_Files`
+--
+
+CREATE TABLE `Video_Files` (
+  `Project_ID` int(200) NOT NULL,
+  `Video_ID` int(200) NOT NULL,
+  `Videos` varbinary(60000) DEFAULT NULL,
+  `Video_Name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `Video_Size` bigint(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `Text_Files`
+--
+ALTER TABLE `Text_Files`
+  ADD PRIMARY KEY (`File_ID`);
+
+--
+-- Indexes for table `Video_Files`
+--
+ALTER TABLE `Video_Files`
+  ADD PRIMARY KEY (`Video_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Text_Files`
+--
+ALTER TABLE `Text_Files`
+  MODIFY `File_ID` int(200) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `Video_Files`
+--
+ALTER TABLE `Video_Files`
+  MODIFY `Video_ID` int(200) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
