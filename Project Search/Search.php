60c8714551ac2529357197b1c446700a53ff259c
ScGill <srg.gilliam@gmail.com>
2017-11-01 14:01:45 -0400

    Add files via upload

---
+++ Project Search/Search.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>ProjectSearch</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+  <body>
+
+  <header>
+  	<nav>
+  		<div class="main-wrapper">
+  			<ul>
+  				<li><a href="index.php">Home &nbsp; &nbsp;</a></li>
+  				<li><a href="index.php"> Student Portal &nbsp; &nbsp;</a></li>
+  				<li><a href="index.php">About Us &nbsp; &nbsp;</a></li>
+  				<li><a href="index.php">Contact Us &nbsp;</a></li>
+
+			   </ul>
+      </div>
+    </nav>
+  </header>
+  </html>
